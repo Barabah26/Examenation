42889209e806eb2df6b0f1a4cac7659d9f84d387
Pavlo Barabah <99317379+Barabah26@users.noreply.github.com>
2022-06-03 11:43:49 +0300

    Add files via upload

---
+++ Task2/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        One one = new One();
+        System.out.println("Добуток дорівнює: " + one.mnosh());
+    }
+}
---
+++ Task2/src/com/company/One.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class One {
+    int mnosh() {
+        int j = 1;
+        for (int i = 1; i <= 5; i++) {
+            j = j * i;
+        }
+        return j;
+    }
+}
